4a0256028c8bacbdb5e8aed9588c69008e6ac733
Alvaro Balboa <souporman@Alvaros-MacBook-Pro.local>
2017-03-22 22:48:03 -0400

    This is a snapchat clone that has all the objects on one page

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+//THIS IS A SMALL FUNCTIONALITY TO SHOW USER LOGIN AND LOG OUT STUFF
 
 function getUser(userData) {
 
@@ -12,15 +13,34 @@ function getUser(userData) {
 
 $.get("/user", getUser);
 
-
+//THIS IS GETTING A LIST OF PHOTOS
 
 function getPhotos(photosData) {
     for (var i in photosData) {
-        var photo = photosData[i];
-        var elem = $("<img>");
-        elem.attr("src", photo.filename);
+        var elem = $("<a>");
+        elem.attr("href", "photosdisplay/" + photosData[i].filename);
+        elem.text(photosData[i].filename);
         $("#photos").append(elem);
+        var elem2 = $("<br>");
+        $("#photos").append(elem2);
     }
 }
 
 $.get("/photos", getPhotos);
+
+//THIS IS FOR DISPLAYING THE ACTUAL PHOTOS
+
+function showPhoto(photoData) {
+        var photo = photoData;
+        var elem = $("<img>");
+        elem.attr("src", photo.filename,"height=&#822042&#8221 width=&#822042&#8221");
+        $("#photosdisplay").append(elem);
+}
+
+$.get("/sessionPhoto",showPhoto);
+
+$.get("/delete");
+
+
+//THIS WAS SET TO RELOAD PAGE AUTOMATICLY BUT IT RELOADS TO FAST
+// $.get("/reload",location.reload(false));
--- src/main/java/com/theironyard/novauc/entities/Photo.java
+++ src/main/java/com/theironyard/novauc/entities/Photo.java
@@ -21,13 +21,21 @@ public class Photo {
     @Column(nullable = false)
     String filename;
 
+    @Column(nullable = false)
+    int delay= 10;
+
+    @Column(nullable = false)
+    boolean publicPhoto =false;
+
     public Photo() {
     }
 
-    public Photo(User sender, User recipient, String filename) {
+    public Photo(User sender, User recipient, String filename, int delay, boolean publicPhoto) {
         this.sender = sender;
         this.recipient = recipient;
         this.filename = filename;
+        this.delay = delay;
+        this.publicPhoto = publicPhoto;
     }
 
     public int getId() {
@@ -60,6 +68,22 @@ public class Photo {
 
     public void setFilename(String filename) {
         this.filename = filename;
+    }
+
+    public int getDelay() {
+        return delay;
+    }
+
+    public void setDelay(int delay) {
+        this.delay = delay;
+    }
+
+    public boolean isPublicPhoto() {
+        return publicPhoto;
+    }
+
+    public void setPublicPhoto(boolean publicPhoto) {
+        this.publicPhoto = publicPhoto;
     }
 }
 
